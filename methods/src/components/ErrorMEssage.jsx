d490d63e221955361afce60a465671d12fcfa9a2
shoyab <shoyabkhan0508@gmail.com>
2024-08-21 00:24:07 -0700

    css module

--- methods/src/components/ErrorMEssage.jsx
+++ methods/src/components/ErrorMEssage.jsx
@@ -1,16 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function ErrorMEssage({items}) {
-
-   
-  return (
-    <div>
-      {
-        items.length === 0 && <h3>I am still hungary !</h3>
-      }
-
-    </div>
-  )
+function ErrorMEssage({ items }) {
+  return <div>{items.length === 0 && <h3>I am still hungary !</h3>}</div>;
 }
 
-export default ErrorMEssage
+export default ErrorMEssage;
