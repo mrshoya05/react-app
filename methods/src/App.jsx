c4763f86d40708fe081042f72ed8991d9db207dc
shoyab <shoyabkhan0508@gmail.com>
2024-08-23 23:50:57 -0700

    child props !

--- methods/src/App.jsx
+++ methods/src/App.jsx
@@ -1,20 +1,24 @@
-
-import './App.css'
-import Conditionalrendering from './components/Conditionalrendering'
-import ErrorMEssage from './components/ErrorMEssage'
+import "./App.css";
+import Conditionalrendering from "./components/Conditionalrendering";
+import Cotainer from "./components/Cotainer";
+import ErrorMEssage from "./components/ErrorMEssage";
 
 function App() {
   // let foodItems = []
-         let foodItems = ["dal", "curry", "paratha", "sabji-Roti", "special-thali"]
+  let foodItems = ["dal", "curry", "paratha", "sabji-Roti", "special-thali"];
 
   return (
-    < >
-  <h1 className='food-heading'>Healthy food !</h1>
-  <Conditionalrendering items={foodItems} />
-  <ErrorMEssage items={foodItems} />
-
+    <>
+      <Cotainer>
+        <h1 className="food-heading">Healthy food !</h1>
+        <Conditionalrendering items={foodItems} />
+        <ErrorMEssage items={foodItems} />
+      </Cotainer>
+      <Cotainer>
+        <p>Hello here are list of some Healthy food !</p>
+      </Cotainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- methods/src/components/Items.jsx
+++ methods/src/components/Items.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import styles from './Items.module.css'
+import React from "react";
+import styles from "./Items.module.css";
 
-function Items({foodItems}) {
-    console.log(styles);
-    
+function Items({ foodItems }) {
+  console.log(styles);
+
   return (
     <>
-  <li  className={`${styles['kg-items']}`}><span className={styles['kg-span']}>{foodItems}</span></li>
+      <li className={`${styles["kg-items"]} list-group-item`}>
+        <span className={styles["kg-span"]}>{foodItems}</span>
+      </li>
     </>
-
-     
-   
-  )
+  );
 }
 
-export default Items
+export default Items;
